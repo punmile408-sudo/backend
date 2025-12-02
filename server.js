29e00c59745916e910b53f5e4638a6fb705d7807
unknown <punmile408@gmail.com>
2025-12-02 19:17:01 +0700

    Updata mongoDB

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
+const User = require('./models/User')
 const app = express()
 
 app.use(cors({ origin: true }))
 app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
+
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find()
+        res.json(users)
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+})
 
 app.get('/', (req, res) => {
     res.json({ message: 'Server can run from punmile' })
