a21e404f15b75b9f755a357d3faecfb1b394ad64
unknown <punmile408@gmail.com>
2025-12-05 16:07:00 +0700

    Vercel

--- server.js
+++ server.js
@@ -33,5 +33,11 @@ app.post('/register', async (req, res) => {
     }
 })
 
-const PORT = process.env.PORT || 8000
-app.listen(PORT, () => console.log('Server running on port ' + PORT))
+module.exports = app
+
+if (require.main === module) {
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}
